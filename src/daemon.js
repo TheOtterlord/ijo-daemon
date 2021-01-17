7edf4bfd728f6988793d4aecfa34a36753e0aa23
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2021-01-17 14:01:18 +0100

    Add docs

--- src/daemon.js
+++ src/daemon.js
@@ -3,21 +3,43 @@ const {ConfigFile} = require("ijo-utils");
 const PanelHandler = require("./net/panel/handler");
 const AuthHandler = require("./auth/handler");
 
+/**
+ * This is the core class for the daemon for IJO. It initializes, starts and stops the daemon.
+ */
 class Daemon {
     constructor() {
+        /**
+         * The configuration file containing the options for the daemon.
+         * @type {ConfigFile}
+         */
         this.config = new ConfigFile(path.join(this.root, "./config.json"), {defaults: {
             name: "test",
             panel: {host: "localhost", port: 8081},
             auth: {}
         }});
+        /**
+         * The handler that handles the connection with the panel.
+         * @type {PanelHandler}
+         */
         this.panelHandler = new PanelHandler();
+        /**
+         * The handler that authenticates the daemon with the panel.
+         */
         this.authHandler = new AuthHandler();
     }
 
+    /**
+     * Returns the the root directory for the daemon.
+     * @returns {String} The root directory.
+     */
     get root() {
         return path.join(path.dirname(require.main.filename), "../");
     }
 
+    /**
+     * Initializes the daemon.
+     * @returns {Promise} A promise that resolves when the daemon has been initialized.
+     */
     async initialize() {
         await this.config.load().catch(e => {throw e});
         this.name = this.config.get("name");
@@ -26,6 +48,10 @@ class Daemon {
         this.authHandler.initialize({panelHandler: this.panelHandler, config: this.config.get("auth")});
     }
 
+    /**
+     * Starts the daemon.
+     * @returns {Promise} A promise that resolves when the daemon has started.
+     */
     async start() {
         await this.panelHandler.connect().catch(e => {throw e});
         this.authHandler.authenticate({
@@ -33,6 +59,10 @@ class Daemon {
         });
     }
 
+    /**
+     * Stops the daemon.
+     * @returns {Promise} A promise that resolves when the daemon has stopped.
+     */
     async stop() {
         await this.panelHandler.close().catch(e => {throw e});
         await this.config.save().catch(e => {throw e});
